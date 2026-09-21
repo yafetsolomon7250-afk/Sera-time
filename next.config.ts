3fa867e1212cb5a120c3dc1d1f52fceee332cd4d
yafetsolomon7250-afk <yafetsolomon7250@gmail.com>
2026-09-21 22:03:28 +0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
-};
-
-export default nextConfig;
