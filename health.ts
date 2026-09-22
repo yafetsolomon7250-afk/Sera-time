eeb677764404d5f37887150ab6d3ba1c9ea8f40b
yafetsolomon7250-afk <yafetsolomon7250@gmail.com>
2026-09-22 20:03:05 +0300

    Add files via upload

---
+++ health.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    ok: true,
+    service: "sera-time",
+    version: "9.0",
+    time: new Date().toISOString(),
+  });
+}
