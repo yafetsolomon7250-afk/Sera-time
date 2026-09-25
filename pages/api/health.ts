70bd8c3c8fbcb41120801519102e9a0a0ed6dd94
yafetsolomon7250-afk <yafetsolomon7250@gmail.com>
2026-09-25 13:33:09 +0300

    Delete pages/api directory

--- pages/api/health.ts
+++
@@ -1,4 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ ok: true, service: "sera-time", version: "11.0", time: new Date().toISOString() });
-}
